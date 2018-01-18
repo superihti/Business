7ec7fe77d52e1577b68f917c2a1b626929e3da2a
superihti <superihti@yandex.ru>
2018-01-19 00:54:05 +0300

    lesson one

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,14 @@
+'use strict';
+
 var gulp        = require('gulp');
 var browserSync = require('browser-sync').create();
+var pug         = require('gulp-pug');
+var sass        = require('gulp-sass');
+var spritesmith = require('gulp.spritesmith');
+var rimraf      = require('rimraf');
+var rename      = require("gulp-rename");
 
-// Static server
+// ---------------browserSync-----------------------
 gulp.task('server', function() {
     browserSync.init({
         server: {
@@ -11,3 +18,48 @@ gulp.task('server', function() {
     });
     gulp.watch('build/**/*').on('change', browserSync.reload);
 });
+
+// ------------------Pug----------------------------
+gulp.task('pug', function buildHTML() {
+  return gulp.src('source/template/index.pug')
+  .pipe(pug({
+    pretty: true 
+  }))
+  .pipe(gulp.dest('build'))
+});
+
+//---------------Sass--------------------------------
+gulp.task('sass', function () {
+  return gulp.src('source/style/style.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(rename('style.min.css'))
+    .pipe(gulp.dest('build/css'));
+});
+ 
+
+//---------------Sprite-----------------------------
+gulp.task('sprite', function () {
+  var spriteData = gulp.src('sourse/img/*.png').pipe(spritesmith({
+    imgName: 'sprite.png',
+    cssName: 'sprite.sass'
+  }));
+  return spriteData.pipe(gulp.dest('build/img'));
+});
+
+//----------------Delete-------------------------
+gulp.task('clean', function del(cb) {
+  return rimraf('build', cb);
+});
+
+/* ------------ Watchers ------------- */
+gulp.task('watch', function() {
+  gulp.watch('source/template/**/*.pug', gulp.series('pug'));
+  gulp.watch('source/style/**/*.scss', gulp.series('sass'));
+});
+
+gulp.task('default', gulp.series(
+  'clean',
+  gulp.parallel('pug', 'sass', 'sprite'),
+  gulp.parallel('watch', 'server')
+  )
+);
